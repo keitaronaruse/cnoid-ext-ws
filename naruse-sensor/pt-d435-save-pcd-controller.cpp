a8f1331e0312149fe618e20935e5935268ff6e9d
keitaronaruse <naruse@u-aizu.ac.jp>
2021-07-17 13:40:17 +0900

    updated

--- naruse-sensor/pt-d435-save-pcd-controller.cpp
+++ naruse-sensor/pt-d435-save-pcd-controller.cpp
@@ -1,10 +1,10 @@
 /*
-    naruse-d435-save.cpp
-        Simple controller for d435 range camera
+    pt-d435-save.cpp
+        Simple controller for d435 range camera with pan and tilt control
+            - Pan angle: +Delta by L button, -Delta by   
             - Save an image file and pcd one when A button is pushed down
-            - Point cloud is given by D435 sensor frame
         Author: Keitaro Naruse
-        Date:   2021-06-19
+        Date:   2021-07-17
 */
 //  For file save in c++ language
 #include <iostream>
@@ -12,12 +12,10 @@
 
 //  For choreonoid
 #include <cnoid/SimpleController>
-#include <cnoid/Camera>
 #include <cnoid/RangeCamera>
 #include <cnoid/Joystick>
 
-//  pcl related header files
-//  For basic IO
+//  For pcl
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 //  For frame transformation
@@ -26,23 +24,34 @@
 #include <pcl/filters/passthrough.h>
 
 /*
-    class NaruseD435SaveController {}
+    class PTD435SavePCDController {}
         Simple controller for Naruse D435 Range Camera
             Save an image file and a PCD file 
             when A-button of joystick is pressed down
 */
-class NaruseD435SaveController : public cnoid::SimpleController
+class PTD435SavePCDController : public cnoid::SimpleController
 {
 private:
     //  Class instance 
+    //  SimpleController
     cnoid::SimpleControllerIO* io;
+    //  Joystick
     cnoid::Joystick joystick;
+    //  RangeCamera
     cnoid::RangeCamera* d435RangeCamera;
+    //  Button flag
     bool d435PrevButtonState;
+    //  Joint
+    cnoid::Link* panPlate;
+    cnoid::Link* tiltPlate;
 
-    //  Make a colored point cloud of a current scene in the choreonoid frame
+    //  Reference angle
+    double qref_pan;
+    double qref_tilt;
+
+    //  Make a colored point cloud of a current scene
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr makeColoredPointCloudOfCurrentSceneInCnoidFrame() {
-        // Make an instance of a point cloud
+         // Make an instance of a point cloud
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr pc_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
 
         //  Image width and height
@@ -58,7 +67,7 @@ private:
         pc_ptr -> height   = 1;
         pc_ptr -> is_dense = false;
         pc_ptr -> points.resize(pc_ptr -> width * pc_ptr -> height);
-        
+
         //  Conver a 3D point in RangeCamera and color in Camera to a colored point cloud
         //  Each point i in cloud is represented as
         //      cloud[i].x, cloud[i].y, cloud[i].z: (x,y,z) position 
@@ -78,8 +87,9 @@ private:
             pc_ptr -> points[i].b = pixels[3*i + 2];
             ++i;
         }
+
         return(pc_ptr);
-    }
+    };
 
     //  Convert a colored point cloud from choreonoid frame to D435 frame
     //  Reference: https://pointclouds.org/documentation/tutorials/matrix_transform.html 
@@ -129,17 +139,32 @@ public:
         io->enableInput(d435RangeCamera);
         d435PrevButtonState = false;
 
+        //  Set pan and tilt joints
+        panPlate = io->body()->link("PanPlate");
+        tiltPlate = io->body()->link("TiltPlate");
+        panPlate -> setActuationMode(cnoid::Link::JointDisplacement);
+        tiltPlate -> setActuationMode(cnoid::Link::JointDisplacement);
+        io->enableOutput(panPlate);
+        io->enableOutput(tiltPlate);
+        //  Set initial reference angles of pan and tilt
+        qref_pan = 0.0;
+        qref_tilt = 0.0;
+
         return true;
     }
 
     virtual bool control() override
     {
+        //  Read joystick status 
         joystick.readCurrentState();
+        
+        //  Set output stream
+        std::ostream& os = io->os();
+
+        //  Check A button for save files
         bool stateChanged = false;
         bool buttonState = joystick.getButtonState(cnoid::Joystick::A_BUTTON);
         if(buttonState && !d435PrevButtonState){
-            //  Set os to choreonoid message window
-            std::ostream& os = io->os();
             //  Get a cuurent scene as image 
             // const cnoid::Image& d435RGBImage = d435RangeCamera->constImage();
             //  Save it as an image file
@@ -154,13 +179,38 @@ public:
             pcl::io::savePCDFileBinaryCompressed("test-cnoid-frame.pcd", *pcPtrCnoidFrame);
             pcl::io::savePCDFileBinaryCompressed("test-pass-through-cnoid-frame.pcd", *pcPtrPassThroughCnoidFrame);
             pcl::io::savePCDFileBinaryCompressed("test-pass-through-d435-frame.pcd", *pcPtrPassThroughD435Frame);
-            // pcl::io::savePCDFileASCII ("test-image.pcd", *pcPtrCnoidFrame);
+            pcl::io::savePCDFileASCII ("test-pass-through-d435-frame.ascii.pcd", *pcPtrPassThroughD435Frame);
             os << "Saved pcd files" << std::endl;
         }
         d435PrevButtonState = buttonState;
+        
+        //  L key, Rotate pan negative
+        if(joystick.getPosition(2) > 0.5)  {
+            qref_pan -= 0.001;
+            os << "qref_pan[deg]:" << qref_pan/3.14*180.0 << std::endl;
+        }
+        //  J key, Rotate pan positive
+        else if(joystick.getPosition(2) < -0.5) {
+            qref_pan += 0.001;
+            os << "qref_pan[deg]:" << qref_pan/3.14*180.0 << std::endl;
+        } 
+        //  K key, Rotate tilt positive
+        if(joystick.getPosition(3) > 0.5)  {
+            qref_tilt += 0.001;
+            os << "qref_tilt[deg]:" << qref_tilt/3.14*180.0 << std::endl;
+        }
+        //  I key, Rotatetilt negative
+        else if(joystick.getPosition(3) < -0.5) {
+            qref_tilt -= 0.001;
+            os << "qref_tilt[deg]:" << qref_tilt/3.14*180.0 << std::endl;
+        } 
+
+        //  Acutation by angle
+        panPlate -> q_target() = qref_pan;
+        tiltPlate -> q_target() = qref_tilt;
 
         return true;
     }
 };
 
-CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(NaruseD435SaveController)
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(PTD435SavePCDController)
